cf186f63e0bb65904166de958e57b296f2f950af
Mish <michelle.reiter@bluewin.ch>
2021-04-27 18:26:11 +0200

    gameroom repository integration tests #41

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/repository/GameroomRepositoryIntegrationTest.java
@@ -0,0 +1,61 @@
+package ch.uzh.ifi.hase.soprafs21.repository;
+
+import ch.uzh.ifi.hase.soprafs21.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs21.entity.Gameroom;
+import ch.uzh.ifi.hase.soprafs21.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class GameroomRepositoryIntegrationTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Qualifier("gameroomRepository")
+    @Autowired
+    private GameroomRepository gameroomRepository;
+
+    @Test
+    public void findByRoomname_success() {
+        // given
+        Gameroom gameroom = new Gameroom();
+        gameroom.setRoomname("test");
+        gameroom.setPassword("123");
+
+        entityManager.persist(gameroom);
+        entityManager.flush();
+
+        // when
+        Gameroom found = gameroomRepository.findByRoomname(gameroom.getRoomname());
+
+        //then
+        assertEquals(found.getRoomname(), gameroom.getRoomname());
+        assertEquals(found.getPassword(), gameroom.getPassword());
+    }
+
+    @Test
+    public void getOne_success() {
+        // given
+        Gameroom gameroom = new Gameroom();
+        gameroom.setRoomname("test");
+        gameroom.setPassword("123");
+
+        entityManager.persist(gameroom);
+        entityManager.flush();
+
+        // when
+        Gameroom found = gameroomRepository.getOne(gameroom.getId());
+
+        //then
+        assertEquals(found.getRoomname(), gameroom.getRoomname());
+        assertEquals(found.getPassword(), gameroom.getPassword());
+
+    }
+}

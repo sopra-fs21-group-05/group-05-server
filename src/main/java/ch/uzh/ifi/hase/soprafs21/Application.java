d81f3e7fbbf3b8b25fb73d0ac205338823118563
Roman Stadler <rosta99x@gmx.ch>
2021-05-18 20:52:25 +0200

    No one saw this easteregg

--- src/main/java/ch/uzh/ifi/hase/soprafs21/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/Application.java
@@ -21,7 +21,7 @@ public class Application {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public String helloWorld() {
-        return "The application is running.";
+        return "The Pictures Server is running, have fun playing :)";
     }
 
     @Bean

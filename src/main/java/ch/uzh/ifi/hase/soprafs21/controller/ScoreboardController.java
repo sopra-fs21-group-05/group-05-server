3497998c80d0081654b24656b740ea55ae4461fc
Mish <michelle.reiter@bluewin.ch>
2021-05-18 11:04:58 +0200

    send usernames with scoreboard #71

--- src/main/java/ch/uzh/ifi/hase/soprafs21/controller/ScoreboardController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/controller/ScoreboardController.java
@@ -10,22 +10,27 @@ import ch.uzh.ifi.hase.soprafs21.rest.dto.ScoreboardGetDTO;
 import ch.uzh.ifi.hase.soprafs21.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs21.service.GameService;
 import ch.uzh.ifi.hase.soprafs21.service.ScoreboardService;
+import ch.uzh.ifi.hase.soprafs21.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ScoreboardController {
     private final ScoreboardService scoreboardService;
     private final GameService gameService;
+    private final UserService userService;
 
-    ScoreboardController(ScoreboardService scoreboardService, GameService gameService){
+    ScoreboardController(ScoreboardService scoreboardService, GameService gameService, UserService userService){
         this.scoreboardService = scoreboardService;
         this.gameService = gameService;
+        this.userService = userService;
     }
 
     @GetMapping("/scoreboards/{gameId}")
@@ -37,6 +42,13 @@ public class ScoreboardController {
         //get scoreboard
         Scoreboard scoreboard = scoreboardService.findScoreboardByGame(game);
         ScoreboardGetDTO foundScoreboard = DTOMapper.INSTANCE.convertEntityToScoreboardGetDTO(scoreboard);
+        //add map with usernames
+        Map<Long, String> userNames = new HashMap<>();
+        for (Long id: foundScoreboard.getUserPoints().keySet()) {
+            String name = userService.getExistingUser(id).getUsername();
+            userNames.put(id,name);
+        }
+        foundScoreboard.setUserNames(userNames);
 
         return foundScoreboard;
 
--- src/main/java/ch/uzh/ifi/hase/soprafs21/rest/dto/ScoreboardGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/rest/dto/ScoreboardGetDTO.java
@@ -6,10 +6,14 @@ public class ScoreboardGetDTO {
 
     private Long scoreboardId;
     private Map<Long,Integer> userPoints;
+    private Map<Long, String> userNames;
 
     public Long getScoreboardId() { return scoreboardId; }
     public void setScoreboardId(Long scoreboardId) { this.scoreboardId = scoreboardId; }
 
     public Map<Long, Integer> getUserPoints() { return userPoints; }
     public void setUserPoints(Map<Long, Integer> userPoints) { this.userPoints = userPoints; }
+
+    public Map<Long, String> getUserNames() { return userNames; }
+    public void setUserNames(Map<Long, String> userNames) { this.userNames = userNames; }
 }
